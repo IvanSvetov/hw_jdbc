c49420639753f915ba4cb661e1b3538e5b67a54f
Ivan Svetov <ivan_lbov@mail.ru>
2023-04-07 18:35:25 +0500

    hw is done, check it

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,20 +1,29 @@
+import service.Employee;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myPersistenceUnit");
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("myPersistenceUnit");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         entityManager.getTransaction().begin();
 
-        /*String jpqlQuery = "SELECT s FROM Employee s";
+
+
+
+        String jpqlQuery = "SELECT s FROM Employee s";
         TypedQuery<Employee> query = entityManager.createQuery(jpqlQuery, Employee.class);
         List<Employee> employees = query.getResultList();
 
         entityManager.getTransaction().commit();
+
 
         for (Employee employee : employees) {
             System.out.println("Employee ID: " + employee.getId());
@@ -22,7 +31,7 @@ public class Application {
             System.out.println("Employee gender: " + employee.getGender());
             System.out.println("Employee age: " + employee.getAge());
             System.out.println("-----------------");
-        }*/
+        }
 
         entityManager.close();
         entityManagerFactory.close();
